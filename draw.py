edccbefb025191deb2eb6cd99567354b29000584
Frederic <chaotrax@gmail.com>
2022-12-07 11:16:30 +0100

    updated gitignore, added plotly, removed matplolib

--- draw.py
+++ draw.py
@@ -1,10 +1,8 @@
-# class that draws the circles
-import matplotlib as mpl
-import matplotlib.pyplot as plt
+# script that draws the circles
+import plotly
 import numpy as np
 
 
 # Draw my sh&t
 def draw(points):
-    plt.plot(points)
-    plt.show()
+    print("hi")
